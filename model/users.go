88673249eb7147de512b4575a6456273185b822b
HN721 <hosea1422@gmail.com>
2025-06-25 16:44:22 +0700

    feat: create func login in model

--- model/users.go
+++ model/users.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"context"
+	"fmt"
 	"weekly1/utils"
 )
 
@@ -21,4 +22,28 @@ func Register(user Profile) error {
 	}
 	_, err = conn.Exec(context.Background(), `INSERT INTO users (name,email,images,pasword)`, user.Name, user.Email, user.Images, user.Password)
 	return err
+}
+func Login(user Profile) (Profile, error) {
+	conn, err := utils.DBConnect()
+	if err != nil {
+		return Profile{}, err
+	}
+	defer conn.Conn().Close(context.Background())
+
+	var dbUser Profile
+
+	err = conn.QueryRow(context.Background(),
+		`SELECT id, name, email, images, password FROM users WHERE email = $1`,
+		user.Email,
+	).Scan(&dbUser.Id, &dbUser.Name, &dbUser.Email, &dbUser.Images, &dbUser.Password)
+
+	if err != nil {
+		return Profile{}, fmt.Errorf("email tidak ditemukan")
+	}
+
+	if user.Password != dbUser.Password {
+		return Profile{}, fmt.Errorf("password salah")
+	}
+
+	return dbUser, nil
 }
