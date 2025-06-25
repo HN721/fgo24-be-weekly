e4aa334dafabec0e68975318f6828820e45c1520
HN721 <hosea1422@gmail.com>
2025-06-25 16:40:17 +0700

    feat: create func register in model

--- model/users.go
+++ model/users.go
@@ -1,9 +1,24 @@
 package model
 
+import (
+	"context"
+	"weekly1/utils"
+)
+
 type Profile struct {
 	Id       int    `json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Images   string `json:"images"`
 	Password string `json:"password"`
+}
+
+func Register(user Profile) error {
+	conn, err := utils.DBConnect()
+	defer conn.Conn().Close(context.Background())
+	if err != nil {
+
+	}
+	_, err = conn.Exec(context.Background(), `INSERT INTO users (name,email,images,pasword)`, user.Name, user.Email, user.Images, user.Password)
+	return err
 }
