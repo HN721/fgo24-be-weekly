f6fb0a157092483d1bf4ac586bd71f5586905a36
HN721 <hosea1422@gmail.com>
2025-06-27 20:06:33 +0700

    feat: create TopupController

--- controller/balance.go
+++ controller/balance.go
@@ -27,7 +27,30 @@ func BalanceController(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, model.Response{
 		Success: true,
-		Message: "Sucessfully TopUp Data ",
+		Message: "Sucessfully Create Balance  Data ",
 		Results: input,
+	})
+}
+func TopUpController(ctx *gin.Context) {
+	var top model.TopUps
+	err := ctx.ShouldBindJSON(&top)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, model.Response{
+			Success: false,
+			Message: "Cannot Insert Data or Invalid Data",
+		})
+		return
+	}
+	err = model.TopUp(top)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, model.Response{
+			Success: false,
+			Message: "Cannot Inset",
+		})
+	}
+	ctx.JSON(http.StatusOK, model.Response{
+		Success: true,
+		Message: "Sucessfully TopUp Data ",
+		Results: top,
 	})
 }
