100ed17f3e516b286ad46523ee57215c071f56ea
HN721 <hosea1422@gmail.com>
2025-06-27 23:00:55 +0700

    feat: create createtransactionController

---
+++ controller/transaction.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"net/http"
+	"weekly1/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateTransactionsController(ctx *gin.Context) {
+	var input model.Transactions
+
+	err := ctx.ShouldBindJSON(&input)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, model.Response{
+			Success: false,
+			Message: "Cannot Insert Data or Invalid Data",
+		})
+		return
+	}
+}
--- model/transaction.go
+++ model/transaction.go
@@ -3,14 +3,16 @@ package model
 import (
 	"context"
 	"weekly1/utils"
+
+	"github.com/jackc/pgx/v5"
 )
 
 type Transactions struct {
-	amount  int
-	status  string
-	date    string
-	method  string
-	id_user int
+	Amount int    `json:"amount"`
+	Status string `json:"status"`
+	Date   string `json:"date"`
+	Method string `json:"method"`
+	IdUser int    `json:"id_user"`
 }
 
 func CreateTransactions(trans Transactions) error {
@@ -20,7 +22,32 @@ func CreateTransactions(trans Transactions) error {
 	}
 	_, err = conn.Exec(context.Background(), `INSERT INTO transaction (amount,status,date_transaction,method,id_user)VALUES(
 		$1, $2,$3,$4,$5
-	)`, trans.amount, trans.status, trans.date, trans.method, trans.id_user)
+	)`, trans.Amount, trans.Status, trans.Date, trans.Method, trans.IdUser)
 
 	return err
+}
+func GetTransactionByUserId(userId int) ([]Transactions, error) {
+	conn, err := utils.DBConnect()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := conn.Query(context.Background(),
+		`SELECT amount, status, date_transaction, method, id_user 
+		FROM transaction 
+		WHERE id_user = $1`, userId)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		conn.Conn().Close(context.Background())
+	}()
+
+	transactions, err := pgx.CollectRows[Transactions](rows, pgx.RowToStructByName)
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return transactions, nil
 }
