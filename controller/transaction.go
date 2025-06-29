ad0e7198f5f30d77281745cd561741abca834df0
HN721 <hosea1422@gmail.com>
2025-06-29 22:20:14 +0700

    feat: refctor code user id using authorizazion

--- controller/transaction.go
+++ controller/transaction.go
@@ -18,4 +18,17 @@ func CreateTransactionsController(ctx *gin.Context) {
 		})
 		return
 	}
+	userId, _ := ctx.Get("userID")
+	err = model.CreateTransactions(userId.(int), input)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, model.Response{
+			Success: false,
+			Message: "Cannot Inset",
+		})
+	}
+	ctx.JSON(http.StatusOK, model.Response{
+		Success: true,
+		Message: "Sucessfully Transfer  ",
+		Results: input,
+	})
 }
